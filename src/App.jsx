8ed1639d629ba7395b559e324d4d6c06354daf32
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-02-09 13:33:42 -0300

    se agrega el archivo App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+
+import './App.css'
+
+function App() {
+  return (
+      <div className="App">
+            <NavBar />
+            <ItemListContainer greeting="¡Bienvenido a nuestra tienda!" />
+        </div>
+  )
+}
+
+export default App
